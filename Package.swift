10e62313d845b99e8058ae6829f3d9e084fb66a3
Rajesh Budhiraja <rajesh.budhiraja@zomato.com>
2025-05-08 18:10:19 +0530

    Update: Jumbo and Nugget

--- Package.swift
+++ Package.swift
@@ -20,8 +20,29 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Nugget",
+<<<<<<< Updated upstream
             url: "https://github.com/BudhirajaRajesh/Nugget/releases/download/1.0.0/Nugget.xcframework.zip",
             checksum: "d5c15910269d5f5c750bfd245fc2a6e522ded5c07c84e0ee46088102257de60f"
+=======
+            url: "https://github.com/BudhirajaRajesh/NuggetSDK/releases/download/0.0.2-Nugget/Nugget.xcframework.zip",
+            checksum: "5cee653c3c62fd569867126d54f1a88237e491cd87265f8edc898d657a195717"
+        ),
+        // Binary targets previously for NuggetInternalDependency, now direct dependencies for NuggetSDK
+        .binaryTarget(
+            name: "NuggetFoundation",
+            url: "https://github.com/BudhirajaRajesh/NuggetSDK/releases/download/0.0.1-NuggetFoundation/NuggetFoundation.xcframework.zip",
+            checksum: "ca1d32c7fb127fbc615221396187b4b972917704bb6c26c7b8b5f3f10c2d2bd5"
+        ),
+        .binaryTarget(
+            name: "NuggetJumbo",
+            url: "https://github.com/BudhirajaRajesh/NuggetSDK/releases/download/0.0.2-NuggetJumbo/NuggetJumbo.xcframework.zip",
+            checksum: "83d9db10df77a0ceaffd7a6eeec97595d77aab8fabae7af29a7849592c5d09c4"
+        ),
+        .binaryTarget(
+            name: "NuggetApiManager",
+            url: "https://github.com/BudhirajaRajesh/NuggetSDK/releases/download/0.0.1-ZApiManager/ZApiManager.xcframework.zip",
+            checksum: "81463946199f2a6c0abc5226ca8282af64b5435c4ad365473440b9a4b25b99ef"
+>>>>>>> Stashed changes
         ),
         .target(
             name: "NuggetSDK",
--- Sources/NuggetSDK/NuggetSDK.swift
+++ Sources/NuggetSDK/NuggetSDK.swift
@@ -8,7 +8,21 @@ public typealias NuggetAuthUserInfo = Nugget.ZChatAuthUserInfo
 public typealias NuggetConversationInfo = Nugget.ZChatConversationInfo
 public typealias NuggetFactory = Nugget.ZChatKitFactory
 public typealias NuggetAuthProviderDelegate = Nugget.ZChatAuthProviderDelegate
+<<<<<<< Updated upstream
 public typealias NuggetPushNotificationsListener = ZChatPushNotificationsListener
+=======
+public typealias NuggetThemeProviderDelegate = Nugget.ZChatCustomThemeProviderDelegate
+public typealias NuggetFontProviderDelegate = Nugget.ZChatCustomFontProviderDelegate
+public typealias NuggetTicketCreationDelegate = Nugget.ZChatTicketCreationHandlerDelegate
+public typealias NuggetPushNotificationsListener = Nugget.ZChatPushNotificationsListener
+public typealias NuggetFontPropertiesMapping = Nugget.FontPropertiesMapping
+public typealias NuggetFontWeights = Nugget.NuggetFontWeights
+public typealias NuggetFontSizes = Nugget.NuggetFontSizes
+public typealias NuggetUserInfo = Nugget.ZChatUserInfo
+public typealias NuggetBusinessContextProviderDelegate = Nugget.ZChatBusinessContextProviderDelegate
+public typealias NuggetChatBusinessContext = Nugget.ZChatBusinessContext
+public typealias NuggetDeeplinkListener = Nugget.ZChatDeeplinkListener
+>>>>>>> Stashed changes
 
 public class NuggetSDK {
     public static let shared = NuggetSDK()
